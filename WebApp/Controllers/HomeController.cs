7eaa6c1e3eee7a8c01e788b857df5bf1da8099a6
Ihor Sukhorada <ihorsukhorada@outlook.com>
2024-12-02 00:23:55 +0100

    Laboratorium 1 (#1)

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -4,15 +4,13 @@ using WebApp.Models;
 
 namespace WebApp.Controllers;
 
+public enum Operator
+{
+    Add, Mul, Sub, Div
+}
+
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         return View();
@@ -20,6 +18,56 @@ public class HomeController : Controller
 
     public IActionResult Privacy()
     {
+        return View();
+    }
+    
+    public IActionResult About()
+    {
+        return View();
+    }
+    
+    public IActionResult Calculator(Operator? op, double? a, double? b)
+    {
+        if (a == null || b == null)
+        {
+            ViewBag.Error = "Parametry a lub b nie może być równe null";
+            return View();
+        }
+
+        switch (op)
+        {
+            case Operator.Add:
+                ViewBag.Result = a + b;
+                break;
+            
+            case Operator.Mul:
+                ViewBag.Result = a * b;
+                break;
+            
+            case Operator.Sub:
+                ViewBag.Result = a - b;
+                break;
+            
+            case Operator.Div:
+                if (b == 0)
+                {
+                    ViewBag.Error = "Nie można dzielić przez zero";
+                    return View();
+                }
+
+                ViewBag.Result = a / b;
+                break;
+            
+            default:
+                ViewBag.Error = "Parameter op nie może być równy null";
+                return View();
+        }
+        
+        ViewBag.Op = op;
+        ViewBag.A = a;
+        ViewBag.B = b;
+        ViewBag.Error = "";
+        
         return View();
     }
 
