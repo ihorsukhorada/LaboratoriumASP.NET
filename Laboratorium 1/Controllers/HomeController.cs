488d1556868d77f406aa792f19b4daded4a1cb35
Ihor Sukhorada <ihorsukhorada@outlook.com>
2025-01-23 16:01:29 +0100

    Add Calculator method

--- Laboratorium 1/Controllers/HomeController.cs
+++ Laboratorium 1/Controllers/HomeController.cs
@@ -22,6 +22,62 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    public IActionResult Calculator(Operator op, double? a, double? b)
+    {
+        if (a == null || b == null)
+        {
+            ViewBag.Error = "Wymagane są oba parametry 'a' i 'b'!";
+            return View();
+        }
+        
+        double result;
+        string sign;
+
+        switch (op)
+        {
+            case Operator.Add:
+                result = a.Value + b.Value;
+                sign = "+";
+                break;
+            case Operator.Sub:
+                result = a.Value - b.Value;
+                sign = "-";
+                break;
+            case Operator.Mul:
+                result = a.Value * b.Value;
+                sign = "*";
+                break;
+            case Operator.Div:
+                if (b.Value != 0)
+                {
+                    result = a.Value / b.Value;
+                    sign = "/";
+                }
+                else
+                {
+                    ViewBag.Error = "Dzielenie przez zero jest niedozwolone!";
+                    return View();
+                }
+                break;
+            case Operator.Unknown:
+            default:
+                ViewBag.Error = "Nieprawidłowa operacja!";
+                return View();
+        }
+
+        ViewBag.A = a.Value;
+        ViewBag.B = b.Value;
+        ViewBag.Sign = sign;
+        ViewBag.Result = result;
+
+        return View();
+    }
+    
+    public enum Operator
+    {
+        Unknown, Add, Mul, Sub, Div
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
